f486ef473edb48fad4417b9cfd0ea6a324db0925
Sune Mølgaard Laursen <smla@dtu.dk>
2016-02-10 14:50:13 +0100

    Cleanup

--- src/main/java/dk/smlaursen/TSNCF/visualization/Visualizer.java
+++ src/main/java/dk/smlaursen/TSNCF/visualization/Visualizer.java
@@ -1,17 +1,12 @@
 package dk.smlaursen.TSNCF.visualization;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
