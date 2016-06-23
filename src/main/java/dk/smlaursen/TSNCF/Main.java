20863762c055020a8869b1bec2dc3976c6fa9cd6
Sune Mølgaard Laursen <smla@dtu.dk>
2016-06-23 11:54:09 +0200

    Fixed comment in TODO list

--- src/main/java/dk/smlaursen/TSNCF/Main.java
+++ src/main/java/dk/smlaursen/TSNCF/Main.java
@@ -36,7 +36,6 @@ public class Main {
 	//FIXME todos
 	//////////////////////////////////////////////
 	//TODO Create pre-processor and validator
-	//TODO Fix so that a GCLEdge takes the union
 	//TODO Improve logging and error handling
 	//TODO Create GUI in separate project
 	//////////////////////////////////////////////
